8f2915d62a6e2cef498c9a8b9cbbeb599fb3f62c
Ludwing Acevedo <ludwing.acevedo.s@gmail.com>
2019-08-20 00:46:41 -0500

    'img3'

--- cicloviaa/urls.py
+++ cicloviaa/urls.py
@@ -24,5 +24,5 @@ from A3 import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('reporte/', views.addpf),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
